886d6da149c8423e5cb70684b674930ecde1413b
diksha-05-qa <diksha@rootsraja.in>
2026-01-08 10:27:44 +0530

    Update test to open OrangeHRM login page

--- First_test.py
+++ First_test.py
@@ -1,18 +1,20 @@
 from playwright.sync_api import sync_playwright
 
-def test_open_google():
+def test_open_orangehrm():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
 
-        page.goto("https://www.google.com")
-        assert "Google" in page.title()
+        page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-        print("Browser Launch successfully!")
+        # Validate page title
+        assert "OrangeHRM" in page.title()
 
-        page.wait_for_timeout(5000)  # keep browser open for 5 sec
+        print("OrangeHRM Login page launched successfully!")
+
+        page.wait_for_timeout(5000)
         browser.close()
 
 if __name__ == "__main__":
-    test_open_google()
+    test_open_orangehrm()
